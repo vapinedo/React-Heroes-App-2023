596687b018e975bc7f0212aea662fbaeb6886ab6
Victor Pinedo <victorpinedomartinez@gmail.com>
2023-09-05 14:06:47 -0500

    add json-server, react-router-dom & bootstrap

---
+++ src/modules/features/heroes/HeroesRoutes.tsx
@@ -0,0 +1,3 @@
+Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+Omnis eos accusamus, dolore aspernatur totam quos doloremque officia itaque nemo at earum.
+accusantium aliquid ducimus, vel error similique expedita? Vel, nam.
